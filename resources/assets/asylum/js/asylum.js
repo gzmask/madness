46d86e20f2a7ef24b4f344d63c5a3194b6cb5585
Gergely Nagy <algernon@madhouse-project.org>
2013-01-27 02:31:30 +0100

    asylum: New theme
    With the new templating code, it is time for a newish theme too. This
    one has no sidebar, has a better font, and is a nicer design in every
    possible way too.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- resources/assets/asylum/js/asylum.js
+++ resources/assets/asylum/js/asylum.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  prettyPrint();
+  $("a[rel=tooltip]").tooltip();
   $(".row-fluid *[rel=tooltip]").tooltip();
   $("a[href=#distro-debian-wheezy]").tab('show');
 });
--- settings.clj
+++ settings.clj
@@ -1,7 +1,8 @@
-{:template {:default "asylum3-main.html",
+{:template {:default "asylum4-main.html",
             :atom "asylum3-atom.xml"},
  :recent-posts {:columns 3
-                :rows 2}
+                :rows 1
+                :total 6}
  :archive-posts {:columns 3}
  :atom {:base-url "https://asylum.madhouse-project.org"
         :title "Asylum Archives"}}
