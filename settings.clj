c23985e3a8a52b3264a174d7cb3d5c14cd75e4d3
Gergely Nagy <algernon@madhouse-project.org>
2013-03-09 14:23:19 +0100

    asylum: Major rearrangement of the site
    Pretty much noone ever used the menu, so lets drop that, and move its
    contents elsewhere: into a small button bar on the top right, and a
    link farm at the bottom.
    
    With this, the main article has considerably less wasted space around
    it, and all important information is readily visible, without having
    to navigate a menu.
    
    The "recent posts" area is also turned off, as recent posts are now
    expanded in the bottom link farm.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- settings.clj
+++ settings.clj
@@ -1,8 +1,9 @@
 {:template {:default "asylum4-main.html",
             :atom "asylum3-atom.xml"},
  :recent-posts {:columns 3
-                :rows 1
-                :total 6}
- :archive-posts {:columns 3}
+                :rows 0
+                :total 10}
+ :archive-posts {:columns 3
+                 :rows -1}
  :atom {:base-url "https://asylum.madhouse-project.org"
         :title "Asylum Archives"}}
