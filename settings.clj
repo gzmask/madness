da3f5c856eabfd340850e562dcfbf226785bb66e
Gergely Nagy <algernon@madhouse-project.org>
2013-12-01 00:32:33 +0100

    asylum: Revert back to asylum5 for now
    The asylum6 design is not complete yet, but we have deployable changes,
    so revert back to v5 for now.
    
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- settings.clj
+++ settings.clj
@@ -1,9 +1,9 @@
-{:template {:default "asylum6-main.html",
+{:template {:default "asylum5-main.html",
             :atom "asylum3-atom.xml"},
  :recent-posts {:columns 3
                 :rows 0
                 :total 10}
- :archive-posts {:columns 1
+ :archive-posts {:columns 3
                  :rows -1}
  :atom {:base-url "http://asylum.madhouse-project.org"
         :title "Asylum Archives"}}
