c2881bd90784e1fe6bda14c51790f807f588b924
Gergely Nagy <algernon@madhouse-project.org>
2013-12-01 00:08:08 +0100

    asylum: Asylum v6 redesign
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- resources/assets/asylum/js/asylum.js
+++ resources/assets/asylum/js/asylum.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
   $(".rel-tooltip").tooltip();
   $(".row-fluid *[rel=tooltip]").tooltip();
   $("a[href=#distro-debian-wheezy]").tab('show');
+
+  $(".madness-share-twitter").attr("href", "https://twitter.com/share?via=algernoone&url=" + encodeURIComponent(document.location.toString()) + "&text=" + encodeURIComponent(document.title));
+  $(".madness-share-google-plus").attr("href", "https://plus.google.com/share?url=" + encodeURIComponent(document.location.toString()));
+  $(".madness-share-flattr").attr
+    ("href",
+     "https://flattr.com/submit/auto?user_id=algernon&title=" + encodeURIComponent(document.title) + "&category=text&url=" + encodeURIComponent(document.location.toString()))
 });
--- settings.clj
+++ settings.clj
@@ -1,9 +1,9 @@
-{:template {:default "asylum5-main.html",
+{:template {:default "asylum6-main.html",
             :atom "asylum3-atom.xml"},
  :recent-posts {:columns 3
                 :rows 0
                 :total 10}
- :archive-posts {:columns 3
+ :archive-posts {:columns 1
                  :rows -1}
  :atom {:base-url "http://asylum.madhouse-project.org"
         :title "Asylum Archives"}}
