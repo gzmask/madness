e32495033f1d4e973a16a100149ee7b41b122483
Gergely Nagy <algernon@madhouse-project.org>
2013-12-01 03:13:35 +0100

    asylum: Switch to v6 theme
    Signed-off-by: Gergely Nagy <algernon@madhouse-project.org>

--- settings.clj
+++ settings.clj
@@ -1,9 +1,10 @@
-{:template {:default "asylum5-main.html",
-            :atom "asylum3-atom.xml"},
+{:template {:default "asylum6-main.html",
+            :atom "asylum3-atom.xml"
+            :base-url "http://asylum.madhouse-project.org"},
  :recent-posts {:columns 3
                 :rows 0
                 :total 10}
- :archive-posts {:columns 3
+ :archive-posts {:columns 1
                  :rows -1}
  :atom {:base-url "http://asylum.madhouse-project.org"
         :title "Asylum Archives"}}
